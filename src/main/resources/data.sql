56376032e0dd4e2daf93acd52a3b91b976180d89
Alkxyly <alkxyly@hotmail.com>
2020-10-01 12:51:28 -0300

    criptografando senha do usuario

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$uPazYQuBcrDjlSzzhD7sv.mwT3jN0Y1FhmIHKmeukih/6BXHdC8Jy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
